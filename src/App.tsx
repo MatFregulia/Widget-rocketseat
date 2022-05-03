f5702c2ac1fbde5567105a9cd2db67c270c0d4ab
Matheus Fregulia <matheus.gviva@gmail.com>
2022-05-03 10:40:58 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-500 px-2 h-10 rounded">{props.text ?? 'Default'}</button>
+}
+
+function App() {
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div> 
+  )
+}
+
+export default App

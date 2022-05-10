ca69f1fa08cbd861530b54111ab48da37da69607
Matheus Fregulia <matheus.gviva@gmail.com>
2022-05-10 09:58:37 -0300

    Criação da funcionalidade do botão FeedbackTypeStep.part_1

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,10 +1,13 @@
-import { CloseButton } from "./CloseButton";
+import { useState } from "react";
 
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import { CloseButton } from "../CloseButton";
 
-const feedbackTypes = {
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+export const feedbackTypes = {
     BUG: {
         title: "Problema",
         image: {
@@ -26,20 +29,26 @@ const feedbackTypes = {
             alt: 'Imagem de um balão de pensamento'
         }
     },
-}
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
     return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
                 <span className="text-xl leading-6">Deixe seu Feedback</span>
 
                 <CloseButton/>
             </header>
 
-            <div className="flex py-8 gap-2 w-full-width">
-                <button></button>
-            </div>
+            { !feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <p>Hello World</p>
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito com ❤️ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
